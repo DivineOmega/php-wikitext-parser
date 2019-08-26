69f370797563bd0684b4e9e133ebfbaf313888d4
Jordan Hall <jordan@hall05.co.uk>
2019-08-26 10:25:35 +0100

    Replace new lines with spaces

--- src/Utils.php
+++ src/Utils.php
@@ -7,6 +7,7 @@ class Utils
     public static function stripTagsMaintainWhitespace(string $html)
     {
         $plaintext = $html;
+        $plaintext = str_replace(PHP_EOL, ' ', $plaintext);
         $plaintext = str_replace('<', ' <', $plaintext);
         $plaintext = strip_tags($plaintext);
         $plaintext = str_replace('  ', ' ', $plaintext);
